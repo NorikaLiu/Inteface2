ef83300b43623130c5a39cc826f41eb53efe93ed
NorikaLiu <31222327+NorikaLiu@users.noreply.github.com>
2017-09-25 07:41:37 -1000

    Add files via upload

---
+++ ShoppingCart.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class ShoppingCart implements Cart {
+	int unitPrice;
+	public void addItem(SelectedItem newItem) {
+		
+	}
+	
+	public void deleteItem(int deleteItemNumber) {
+		if (itemNumber == itemNumber) {
+			deleteItem;
+		}
+	public void getTotal (); {
+	
+	int total;
+		for (i = 0; i < Cart; i++) {					
+		for (i = 0; i < Selecteditem; i++)
+		total + unitPrice [i] = total;
+		
+		
+	}
+	}
+
+}
+
